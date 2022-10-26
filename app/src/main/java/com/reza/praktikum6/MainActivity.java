d224b0a4d8f6d0d327cc604806a56e8bb6c0aca2
rezaio <rezaoktario77@gmail.com>
2022-10-26 11:17:37 +0700

    awal commit

---
+++ app/src/main/java/com/reza/praktikum6/MainActivity.java
@@ -0,0 +1,19 @@
+package com.reza.praktikum6;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setTitle("Google Pixel");
+        }
+
+    }
+}
